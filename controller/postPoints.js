a9f2d5c2ef0180512c537c8d12f6691d4637e606
shanawaz-Git <flamefayaz03@gmail.com>
2022-11-27 22:08:10 +0530

    CRUD

--- controller/postPoints.js
+++ controller/postPoints.js
@@ -1,16 +1,23 @@
 const basicInfo = require("../modelSchema/basicInfo");
 exports.upload = (req, res) => {
   const details = new basicInfo({
-    name: req.body.name,
+    name: req.body.name.toLowerCase(),
     email: req.body.email,
     age: req.body.age,
   });
-  const result = details
-    .save()
-    .then(() => {
-      res.send("Saved to basicInfos DB").status(200);
-    })
-    .catch((err) => {
-      res.send("error while saving to DB\n", err);
-    });
+  basicInfo.find({ name: req.body.name.toLowerCase() }, function (err, docs) {
+    if (docs.length) {
+      var data = JSON.stringify(docs);
+      res.send("user already exist\n" + data).status(403);
+    } else {
+      const result = details
+        .save()
+        .then(() => {
+          res.send("Saved to basicInfos DB").status(200);
+        })
+        .catch((err) => {
+          res.send("error while saving to DB\n", err);
+        });
+    }
+  });
 };
