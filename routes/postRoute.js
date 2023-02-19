40e14f8e1c7bde189381e0bc4a9ae9ef5675e684
shanawaz-Git <flamefayaz03@gmail.com>
2023-02-19 16:56:12 +0530

    postSNOW

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,8 +1,9 @@
 "use strict";
 const express = require("express");
-const { fileUpload, nodeSNOW } = require("../controller/postPoints");
+const { fileUpload, nodeSNOWPost } = require("../controller/postPoints");
 const multer = require("multer");
 const postRoute = express.Router();
 const upload = multer();
 postRoute.post("/fileUpload", upload.any(), fileUpload);
+postRoute.post("/SNOW", nodeSNOWPost);
 module.exports = { routes: postRoute };
